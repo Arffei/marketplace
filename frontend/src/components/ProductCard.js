3028b24e0fe31284e1a3c1f49f35764545c87863
Arffei <arffei.storm@gmail.com>
2025-05-01 17:25:04 +0700

    1

---
+++ frontend/src/components/ProductCard.js
@@ -0,0 +1,18 @@
+// frontend/src/components/ProductCard.js
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+
+const ProductCard = ({ title, price, image }) => {
+  return (
+    <Card style={{ width: "18rem", margin: "10px" }}>
+      <Card.Img variant="top" src={image} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{price} руб.</Card.Text>
+        <Button variant="primary">В корзину</Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default ProductCard;
