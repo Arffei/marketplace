b0395d7b853ffc03b7e167ee47d0930c815e1ce2
Arffei <arffei.storm@gmail.com>
2025-05-08 02:30:54 +0700

    2

--- backend/config/auth.php
+++ backend/config/auth.php
@@ -112,4 +112,11 @@ return [
 
     'password_timeout' => env('AUTH_PASSWORD_TIMEOUT', 10800),
 
+    'guards' => [
+    'sanctum' => [
+        'driver' => 'sanctum',
+        'provider' => 'users',
+         ],
+    ],
+
 ];
