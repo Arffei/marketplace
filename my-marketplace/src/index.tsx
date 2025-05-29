83ec77c7387db24fcaefed60a3a9d27dcae70c25
Arffei <arffei.storm@gmail.com>
2025-05-29 14:11:45 +0700

    4

--- my-marketplace/src/index.tsx
+++ my-marketplace/src/index.tsx
@@ -1,19 +1,28 @@
+// src/index.tsx
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './styles/theme';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import ProductDetails from './pages/ProductDetails';
+import Cart from './pages/Cart';
+import Favorite from './pages/Favorite';
+import Account from './pages/Account';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+render(
+  <ThemeProvider theme={theme}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/favorites" element={<Favorite />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
+    </Router>
+  </ThemeProvider>,
+  document.getElementById('root')
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
