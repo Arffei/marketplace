c826bd51c392a8a71aa085c838ae61c3f45b8588
Arffei <arffei.storm@gmail.com>
2025-05-29 14:07:37 +0700

    3

---
+++ my-marketplace/src/styles/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ffa000', // Основной цвет
+    },
+    secondary: {
+      main: '#ffffff', // Второстепенный цвет
+    },
+  },
+});
+
+export default theme;
